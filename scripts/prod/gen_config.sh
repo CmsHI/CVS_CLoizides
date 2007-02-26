40b124d1b1b9e43a6336eeae4e720d93563e212a
loizides <>
2007-02-26 21:11:54 +0000

    Production scripts.

--- scripts/prod/gen_config.sh
+++ scripts/prod/gen_config.sh
@@ -1,42 +1,97 @@
 #!/bin/bash
+# $Id:$
 
-CFGTEMPPATH=$1
 if test -z "$CFGTEMPPATH"; then
-    echo "Config template pathname missing, exiting!"
-    exit 1;
+    CFGTEMPPATH=$1
+    if test -z "$CFGTEMPPATH"; then
+        echo "$0: Config template pathname missing, exiting!"
+        exit 1;
+    fi
 fi
 
-FILENAME=$2
 if test -z "$FILENAME"; then
-    FILENAME=test.root
-    echo "File name missing, setting to $FILENAME!"
+    FILENAME=$2
+    if test -z "$FILENAME"; then
+        FILENAME=test.root
+        echo "$0: File name missing, setting to $FILENAME!"
+    fi
 fi
 
-CFGFILE=$3
 if test -z "$CFGFILE"; then
-    CFGFILE=`echo $CFGTEMPPATH | cut -d_ -f1`_`basename ${FILENAME} .root`.cfg
-    echo "Config name missing, setting to $CFGFILE"
+    CFGFILE=$3
+    if test -z "$CFGFILE"; then
+        CFGFILE=`echo $CFGTEMPPATH | cut -d_ -f1`_`basename ${FILENAME} .root`.cfg
+        echo "$0: Config name missing, setting to $CFGFILE"
+    fi
 fi
 
-EVENTS=$4
 if test -z "$EVENTS"; then
-    EVENTS=1
-    echo "Number of events missing, setting to $EVENTS"
+    EVENTS=$4
+    if test -z "$EVENTS"; then
+        EVENTS=1
+        echo "$0: Number of events missing, setting to $EVENTS"
+    fi
 fi
 
-FEVNUM=$5
 if test -z "$FEVNUM"; then
-    FEVNUM=1
-    echo "Number of first event missing, setting to $FEVNUM"
+    FEVNUM=$5
+    if test -z "$FEVNUM"; then
+        FEVNUM=1
+        echo "$0: Number of first event missing, setting to $FEVNUM"
+    fi
 fi
 
-FRUNNUM=$6
 if test -z "$FRUNNUM"; then
-    FRUNNUM=1
-    echo "Number of run missing, setting to $FRUNNUM"
+    FRUNNUM=$6
+    if test -z "$FRUNNUM"; then
+        FRUNNUM=1
+        echo "$0: Number of run missing, setting to $FRUNNUM"
+    fi
 fi
 
-cat $CFGTEMPPATH | sed "s/__FILENAME__/$FILENAME/"    | sed "s/__EVENTS__/$EVENTS/" \
-             | sed "s/__FIRSTEVENTNUM__/$FEVNUM/" | sed "s/__FIRSTRUNNUM__/$FRUNNUM/"  \
-             | sed "s/__SEED1__/$RANDOM/"         | sed "s/__SEED2__/$RANDOM/"  \
-             | sed "s/__SEED3__/$RANDOM/" > $CFGFILE
+if test -z "$MAXEVENTS"; then
+    MAXEVENTS=$7
+    if test -z "$MAXEVENTS"; then
+        MAXEVENTS=$EVENTS
+        echo "$0: Number of max events missing, setting to $MAXEVENTS"
+    fi
+fi
+
+if test -z "$MAXEVENTS"; then
+    MAXEVENTS=$7
+    if test -z "$MAXEVENTS"; then
+        MAXEVENTS=$EVENTS
+        echo "$0: Number of max events missing, setting to $MAXEVENTS"
+    fi
+fi
+
+if test -z "$PTMIN"; then
+    PTMIN=0
+fi
+if test -z "$PTMAX"; then
+    PTMAX=-1
+fi
+if test -z "$ETAMIN"; then
+    ETAMIN=-5
+fi
+if test -z "$ETAMAX"; then
+    ETAMAX=+5
+fi
+if test -z "$PHIMIN"; then
+    PHIMIN=0
+fi
+if test -z "$PHIMAX"; then
+    PHIMAX=360
+fi
+
+cat $CFGTEMPPATH | sed "s/__FILENAME__/$FILENAME/"    | sed "s/__EVENTS__/$EVENTS/"        \
+                 | sed "s/__FIRSTEVENTNUM__/$FEVNUM/" | sed "s/__FIRSTRUNNUM__/$FRUNNUM/"  \
+                 | sed "s/__MAXEVENTS__/$MAXEVENTS/"  \
+                 | sed "s/__SEED1__/$RANDOM/"         | sed "s/__SEED2__/$RANDOM/"         \
+                 | sed "s/__SEED3__/$RANDOM/"         | sed "s/__SEED4__/$RANDOM/"         \
+                 | sed "s/__SEED5__/$RANDOM/"         | sed "s/__SEED6__/$RANDOM/"         \
+                 | sed "s/__SEED7__/$RANDOM/"         | sed "s/__SEED8__/$RANDOM/"         \
+                 | sed "s/__PTMIN__/$PTMIN/"          | sed "s/__PTMAX__/$PTMAX/"          \
+                 | sed "s/__ETAMIN__/$ETAMIN/"        | sed "s/__ETAMAX__/$ETAMAX/"        \
+                 | sed "s/__PHIMIN__/$PHIMIN/"        | sed "s/__PHIMAX__/$PHIMAX/"        \
+    > $CFGFILE
--- scripts/prod/gen_events.sh
+++ scripts/prod/gen_events.sh
@@ -1,35 +1,55 @@
 #!/bin/bash
+# $Id:$
 
 function usage() {
-    echo "Usage: $0 filename cfg_template_path events run_number"
+    echo "Usage: $0 config_filename [output_filename run_number events cfg_template_path]"
     exit 1
 }
 
-FILENAME=$1
-if test -z "$FILENAME"; then
-    echo "Missing filename (argument 1), exiting!"
+CONFIGFILE=$1
+if test -n "$CONFIGFILE" && test -e "$CONFIGFILE"; then
+    source $1
+else 
+    echo "Missing config filename (argument 1), exiting!"
     usage
 fi
+if test "$CONFIGFILE" = "`basename $CONFIGFILE`"; then
+    CONFIGFILE=`pwd`/$CONFIGFILE
+fi
 
-CFGTEMPPATH=$2
+if test -z "$FILENAME"; then
+    export FILENAME=$2
+    if test -z "$FILENAME"; then
+        echo "Missing filename (argument 2), exiting!"
+        usage
+    fi
+fi
+
+if test -z "$FRUNNUM"; then
+    export FRUNNUM=$3
+    if test -z "$FRUNNUM"; then
+        echo "Missing run number (argument 3), exiting!"
+        usage
+    fi
+fi
+
+if test -z "$EVENTS"; then
+    export EVENTS=$4
+    if test -z "$EVENTS"; then
+        echo "Missing number of events (argument 4), exiting!"
+        usage
+    fi
+fi
+
 if test -z "$CFGTEMPPATH"; then
-    echo "Missing cfg_template_path (argument 2), exiting!"
-    usage
+    export CFGTEMPPATH=$5
+    if test -z "$CFGTEMPPATH"; then
+        echo "Missing cfg_template_path (argument 5), exiting!"
+        usage
+    fi
 fi
 if test "$CFGTEMPPATH" = "`basename $CFGTEMPPATH`"; then
-    CFGTEMPPATH=`pwd`/$CFGTEMPPATH
-fi
-
-EVENTS=$3
-if test -z "$EVENTS"; then
-    echo "Missing number of events (argument 3), exiting!"
-    usage
-fi
-
-FRUNNUM=$4
-if test -z "$FRUNNUM"; then
-    echo "Missing run number (argument 4), exiting!"
-    usage
+    export CFGTEMPPATH=`pwd`/$CFGTEMPPATH
 fi
 
 #this is for running in condor
@@ -38,25 +58,48 @@ if test -z "$HOME"; then
 fi
 
 if test -z "$DATADIR"; then
-    DATADIR=$HOME/data/cms/
+    export DATADIR=$HOME/data/cms/
 fi
 
 if test -z "${_CONDOR_SCRATCH_DIR}"; then
     export _CONDOR_SCRATCH_DIR=/tmp/loizides
 fi
 
+if test -z "$GENCONFIG"; then
+    GENCONFIG=`dirname $CFGTEMPPATH`/gen_config.sh 
+fi
+
+if test -z "$CMSSW_VERSION"; then
+    KEEPPATH=`pwd`
+    source $HOME/setup_cms.sh 1_2_3 prod
+    cd $KEEPPATH
+fi
+
 FILEBASE=`basename $FILENAME .root`
 CFGFILE=`basename $CFGTEMPPATH | cut -d_ -f1`_${FILEBASE}.cfg
 TMPDIR=${_CONDOR_SCRATCH_DIR}/`basename $CFGTEMPPATH | cut -d_ -f1`_`date "+%s"`-$RANDOM
 
-if test -z "$CMSSW_VERSION"; then
-    source $HOME/setup_cms.sh 1_2_3 prod
-fi
+#---
+echo "---Settings in gen_events.sh---"
+echo CONFIGFILE=$CONFIGFILE
+echo FILENAME=$FILENAME
+echo CFGTEMPPATH=$CFGTEMPPATH
+echo EVENTS=$EVENTS
+echo FRUNNUM=$FRUNNUM
+echo HOME=$HOME
+echo DATADIR=$DATADIR
+echo _CONDOR_SCRATCH_DIR=${_CONDOR_SCRATCH_DIR}
+echo GENCONFIG=$GENCONFIG
+echo CMSSW_VERSION=$CMSSW_VERSION
+echo FILEBASE=$FILEBASE
+echo CFGFILE=$CFGFILE
+echo TMPDIR=$TMPDIR
+echo "---gen_events.sh---"
+#---
 
 mkdir -p $TMPDIR
 cd $TMPDIR
 
-GENCONFIG=`dirname $CFGTEMPPATH`/gen_config.sh 
 $GENCONFIG $CFGTEMPPATH $FILENAME $TMPDIR/$CFGFILE $EVENTS 1 $FRUNNUM
 
 time cmsRun $TMPDIR/$CFGFILE < /dev/null > ${FILEBASE}.out 2> ${FILEBASE}.err
