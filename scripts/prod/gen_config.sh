fa5edf1cdd10a04f7b25e87ee1bf9373715cf620
loizides <>
2007-04-25 23:09:01 +0000

    Added PartID.

--- scripts/prod/gen_config.sh
+++ scripts/prod/gen_config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# $Id:$
+# $Id: gen_config.sh,v 1.2 2007/02/26 21:11:54 loizides Exp $
 
 if test -z "$CFGTEMPPATH"; then
     CFGTEMPPATH=$1
@@ -86,7 +86,7 @@ fi
 
 cat $CFGTEMPPATH | sed "s/__FILENAME__/$FILENAME/"    | sed "s/__EVENTS__/$EVENTS/"        \
                  | sed "s/__FIRSTEVENTNUM__/$FEVNUM/" | sed "s/__FIRSTRUNNUM__/$FRUNNUM/"  \
-                 | sed "s/__MAXEVENTS__/$MAXEVENTS/"  \
+                 | sed "s/__MAXEVENTS__/$MAXEVENTS/"  | sed "s/__PARTID__/$PARTID/"        \
                  | sed "s/__SEED1__/$RANDOM/"         | sed "s/__SEED2__/$RANDOM/"         \
                  | sed "s/__SEED3__/$RANDOM/"         | sed "s/__SEED4__/$RANDOM/"         \
                  | sed "s/__SEED5__/$RANDOM/"         | sed "s/__SEED6__/$RANDOM/"         \
