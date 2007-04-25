84a9719273d4e63192e5d30f8332b67a8f89becc
richard <>
2007-04-25 23:28:02 +0000

    This commit was manufactured by cvs2svn to create tag 'MY_CMSSW_1_2_3'.
    Sprout from master 2007-04-25 23:27:58 UTC loizides 'Added gamma jet.'
    Delete:
        InvMassAna/BuildFile
        InvMassAna/interface/MCInvMassAna.h
        InvMassAna/interface/MCParticleAna.h
        InvMassAna/interface/MuonHLTInvMassAna.h
        InvMassAna/interface/MuonInvMassAna.h
        InvMassAna/interface/SimInvMassAna.h
        InvMassAna/interface/SimParticleAna.h
        InvMassAna/src/MCInvMassAna.cc
        InvMassAna/src/MCParticleAna.cc
        InvMassAna/src/MuonHLTInvMassAna.cc
        InvMassAna/src/MuonInvMassAna.cc
        InvMassAna/src/SealModule.cc
        InvMassAna/src/SimInvMassAna.cc
        InvMassAna/src/SimParticleAna.cc
        Utils/BuildFile
        Utils/interface/THistFileService.h
        Utils/plugins/BuildFile
        Utils/plugins/THistFileService.cc
        Utils/src/THistFileService.cc
        scripts/reco/HLTMuon_1_3_0_pre4_03012007.cfg
        scripts/reco/stdreco.cfg

--- Utils/plugins/THistFileService.cc
+++
@@ -1,7 +0,0 @@
-// $Id:$
-
-#include "FWCore/ServiceRegistry/interface/ServiceMaker.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "CLoizides/Utils/interface/THistFileService.h"
-
-DEFINE_FWK_SERVICE(THistFileService);
