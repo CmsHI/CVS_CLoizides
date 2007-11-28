a464e4cae10d52e223a76bc8e77e43261443ca8d
richard <>
2007-11-28 09:15:54 +0000

    This commit was manufactured by cvs2svn to create tag 'My_CMSSW_1_5_4'.
    Sprout from master 2007-11-28 09:15:53 UTC loizides 'Removed warning.'
    Delete:
        EcalAna/BuildFile
        EcalAna/data/ecalana.cfg
        EcalAna/data/ecalsimu.cfg
        EcalAna/interface/EcalAna.h
        EcalAna/src/EcalAna.cc
        EcalAna/src/SealModule.cc
        InvMassAna/BuildFile
        InvMassAna/interface/MCChainAna.h
        InvMassAna/interface/MCInvMassAna.h
        InvMassAna/interface/MCParticleAna.h
        InvMassAna/interface/MuonHLTInvMassAna.h
        InvMassAna/interface/MuonInvMassAna.h
        InvMassAna/interface/SimInvMassAna.h
        InvMassAna/interface/SimParticleAna.h
        InvMassAna/src/MCChainAna.cc
        InvMassAna/src/MCInvMassAna.cc
        InvMassAna/src/MCParticleAna.cc
        InvMassAna/src/MuonHLTInvMassAna.cc
        InvMassAna/src/MuonInvMassAna.cc
        InvMassAna/src/SealModule.cc
        InvMassAna/src/SimInvMassAna.cc
        InvMassAna/src/SimParticleAna.cc
        JetAna/BuildFile
        JetAna/src/JetAna.cc
        JetAna/test/rerunjf.cfg
        JetAna/test/runcalojf.cfg
        MyFixes/BuildFile
        MyFixes/interface/FileCatalog.h
        MyFixes/src/FileCatalog.cc
        RecoCandAlgos/BuildFile
        RecoCandAlgos/plugins/BuildFile
        RecoCandAlgos/plugins/MyTrackSel.cc
        RecoCandAlgos/test/runjf.cfg
        Utils/BuildFile
        Utils/interface/THistFileService.h
        Utils/plugins/BuildFile
        Utils/plugins/THistFileService.cc
        Utils/src/THistFileService.cc
        scripts/myCmsRun
        scripts/prod/GammaJetTriggered.settings
        scripts/prod/PartFlat.settings
        scripts/prod/UpsFlat.settings
        scripts/prod/UpsMuMu.settings
        scripts/prod/UpsMuMuTriggered.settings
        scripts/prod/genGammaJetTriggered_template.cfg
        scripts/prod/genPartFlat_template.cfg
        scripts/prod/genUpsFlat_template.cfg
        scripts/prod/genUpsMuMuTriggered_template.cfg
        scripts/prod/genUpsMuMu_template.cfg
        scripts/prod/gen_config.sh
        scripts/prod/gen_events.sh
        scripts/prod/runpartflat.sh
        scripts/prod/runupsflat.sh
        scripts/reco/mctest.cfg
        scripts/reco/mix.cfg
        scripts/reco/mixhepmc.cfg
        scripts/reco/readExtHepMC.cfg
        scripts/reco/readHepMC.cfg
        scripts/reco/redigi.cfg
        scripts/reco/stdreco.cfg
        scripts/testMixingHepMC.sh

--- EcalAna/src/SealModule.cc
+++
@@ -1,7 +0,0 @@
-// $Id: SealModule.cc,v 1.1 2007/05/28 10:31:20 loizides Exp $
-
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "CLoizides/EcalAna/interface/EcalAna.h"
-
-DEFINE_FWK_MODULE(EcalAna);
